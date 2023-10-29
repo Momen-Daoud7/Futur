9fa9eeb2929f6bac7ed4aa164ebf44449ce57f84
Momen-Daoud7 <momendaoud07@gmail.com>
2023-10-29 13:59:25 +0200

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,46 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "pastel-100": "#f5f8fa",
+        "pastel-800": "#5d7583",
+        "steel-800": "#070b0e",
+        "mint-100": "#93e9d4",
+        "mint-200": "#6ed2ba",
+        "mint-800": "#00392c",
+        white: "#fff",
+        azure: "#e1f6f9",
+        gainsboro: "#d9d9d9",
+        "pastel-300": "#e0e9ef",
+      },
+      spacing: {},
+      fontFamily: {
+        "de-body-bs-400": "Gellix",
+      },
+      borderRadius: {
+        "281xl": "300px",
+        "204xl-8": "223.8px",
+        "7xs": "6px",
+      },
+    },
+    fontSize: {
+      sm: "14px",
+      "3xs": "10px",
+      "13xl": "32px",
+      inherit: "inherit",
+    },
+    screens: {
+      lg: {
+        max: "1200px",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  corePlugins: {
+    preflight: false,
+  },
+};
