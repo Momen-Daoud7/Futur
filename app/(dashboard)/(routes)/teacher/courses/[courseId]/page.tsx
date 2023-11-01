1f636e83f68a2ed6bd3324c756d203fa4b0c0ee1
Momen-Daoud7 <momendaoud07@gmail.com>
2023-11-01 21:31:24 +0200

    Add categoryform

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -14,8 +14,7 @@ import { IconBadge } from "@/components/IconBadge";
 import TitleForm from "./_components/TitleForm";
 import DescriptionForm from "./_components/DescriptionForm";
 import { ImageForm } from "./_components/ImageForm";
-// import { ImageForm } from "./_components/image-form";
-// import { CategoryForm } from "./_components/category-form";
+import { CategoryForm } from "./_components/categoryForm";
 // import { PriceForm } from "./_components/price-form";
 // import { AttachmentForm } from "./_components/attachment-form";
 // import { ChaptersForm } from "./_components/chapters-form";
@@ -90,14 +89,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             <DescriptionForm initialData={course} courseId={course.id} />
 
             <ImageForm initialData={course} courseId={course.id} />
-            {/* <CategoryForm
+            <CategoryForm
               initialData={course}
               courseId={course.id}
               options={categories.map((category) => ({
                 label: category.name,
                 value: category.id,
               }))}
-            // /> */}
+            />
           </div>
           <div className="space-y-6">
             <div>
