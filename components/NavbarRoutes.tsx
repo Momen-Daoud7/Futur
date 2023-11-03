455301f9a844843d799fea92efe59415d7736978
Momen-Daoud7 <momendaoud07@gmail.com>
2023-11-03 09:06:08 +0200

    Finish Categoires ui

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -1,37 +1,47 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import {usePathname,useRouter} from "next/navigation"
-import Link from "next/link"
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
 import { Button } from "./ui/button";
-import {  LogOut } from "lucide-react";
+import { LogOut } from "lucide-react";
+import { SearchInput } from "./SearchInput";
 
 const NavbarRoutes = () => {
   const pathname = usePathname();
-  const router = useRouter()
-  console.log(pathname)
+  const router = useRouter();
+  console.log(pathname);
 
-  const isTeacherPage = pathname?.startsWith("/teacher")
+  const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.startsWith("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ? (
-        <Link href="/">
-          <Button size="sm" variant="ghost">
-            <LogOut className="h-4 w-4 mr-2" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="sm" variant="ghost">
-            Teacher Mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      <UserButton afterSignOutUrl="/" />
-    </div>
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button size="sm" variant="ghost">
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teacher Mode
+            </Button>
+          </Link>
+        )}
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </>
   );
 };
+
 export default NavbarRoutes;
